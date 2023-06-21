512e63e678c7e9af14cb3dcf14fedc4e46959a04
Paila Leela Lahari <134393181+laharipaila@users.noreply.github.com>
2023-06-21 20:16:24 +0530

    Update ex4.c

--- module1/day6/ex4.c
+++ module1/day6/ex4.c
@@ -1,43 +1,55 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 struct Student {
-    char name[50];
-    int age;
+    int rollno;
+    char name[20];
     float marks;
 };
 
-int main() {
-    int n;
+// Function to swap two Student structures
+void swap(struct Student* a, struct Student* b) {
+    struct Student temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-    printf("Enter the number of students: ");
-    scanf("%d", &n);
-
-    struct Student* students = (struct Student*)malloc(n * sizeof(struct Student));
-
-    if (students == NULL) {
-        printf("Memory allocation failed. Exiting program.\n");
-        return 1;
+// Function to perform descending bubble sort based on marks
+void sort_students(struct Student* students, int num_students) {
+    for (int i = 0; i < num_students - 1; i++) {
+        for (int j = 0; j < num_students - i - 1; j++) {
+            if (students[j].marks < students[j + 1].marks) {
+                swap(&students[j], &students[j + 1]);
+             }
+        }
     }
-    for (int i = 0; i < n; i++) {
-        printf("\nEnter details for student %d:\n", i + 1);
-        printf("Name: ");
-        scanf("%s", students[i].name);
-        printf("Age: ");
-        scanf("%d", &(students[i].age));
-        printf("Marks: ");
-        scanf("%f", &(students[i].marks));
-    }
+}
 
-    printf("\nStudent Details:\n");
-    for (int i = 0; i < n; i++) {
-        printf("\nStudent %d\n", i + 1);
+// Function to display all members in the array of structures
+void display_students(const struct Student* students, int num_students) {
+    for (int i = 0; i < num_students; i++) {
+        printf("Student %d:\n", i + 1);
+        printf("Roll No: %d\n", students[i].rollno);
         printf("Name: %s\n", students[i].name);
-        printf("Age: %d\n", students[i].age);
         printf("Marks: %.2f\n", students[i].marks);
+        printf("\n");
     }
+}
 
-    free(students);
+int main() {
+    int num_students = 4;
+    struct Student students[] = {
+        { 1001, "John", 85.5 },
+        { 1002, "Alice", 92.0 },
+        { 1003, "Bob", 78.8 },
+        { 1004, "Emma", 91.2 }
+ };
+
+    // Sort the array of structures in descending order based on marks
+    sort_students(students, num_students);
+
+    // Display all members in the sorted array of structures
+    display_students(students, num_students);
 
     return 0;
 }
