363f5054a2f265b370bc134288ce989e8052ca0b
Paila Leela Lahari <134393181+laharipaila@users.noreply.github.com>
2023-06-21 20:09:24 +0530

    Update ex2.c

--- module1/day6/ex2.c
+++ module1/day6/ex2.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
+#include <string.h>
 
-struct Box {
-    double length;
-    double width;
-    double height;
+struct Student {
+    int rollno;
+    char name[20];
+    float marks;
 };
 
-double calculateVolume(struct Box* boxPtr) {
-    return boxPtr->length * boxPtr->width * boxPtr->height;
-}
-
-double calculateSurfaceArea(struct Box* boxPtr) {
-    return 2 * (boxPtr->length * boxPtr->width + boxPtr->length * boxPtr->height + boxPtr->width * boxPtr->height);
+// Function to initialize all members in the array of structures
+void initialize_students(struct Student* students, int num_students) {
+    for (int i = 0; i < num_students; i++) {
+        students[i].rollno = 0;
+        strcpy(students[i].name, "");
+        students[i].marks = 0.0;
+    }
 }
 
 int main() {
-    struct Box myBox = { 5.0, 3.0, 4.0 };
-    struct Box* boxPtr = &myBox;
+    int num_students = 5;
+    struct Student students[num_students];
 
-    // Accessing members using (*) asterisk and (.) dot operator
-    double volume = (*boxPtr).length * (*boxPtr).width * (*boxPtr).height;
-    double surfaceArea = 2 * ((*boxPtr).length * (*boxPtr).width + (*boxPtr).length * (*boxPtr).height + (*boxPtr).width * (*boxPtr).height);
-    printf("Volume (using (*) and .): %.2f\n", volume);
-    printf("Surface Area (using (*) and .): %.2f\n", surfaceArea);
-
-    // Accessing members using (->) arrow operator (both pointer and dot representation)
-    volume = boxPtr->length * boxPtr->width * boxPtr->height;
-    surfaceArea = 2 * (boxPtr->length * boxPtr->width + boxPtr->length * boxPtr->height + boxPtr->width * boxPtr->height);
-    printf("Volume (using ->): %.2f\n", volume);
-    printf("Surface Area (using ->): %.2f\n", surfaceArea);
+    // Initialize all members in the array of structures
+    initialize_students(students, num_students);
+     // Print the student details
+    for (int i = 0; i < num_students; i++) {
+        printf("Student %d:\n", i + 1);
+        printf("Roll No: %d\n", students[i].rollno);
+        printf("Name: %s\n", students[i].name);
+        printf("Marks: %.2f\n", students[i].marks);
+        printf("\n");
+    }
 
     return 0;
 }
+
+   
